4825689591e93e3a85c17b7a1b601d99c818bb5b
amar_singh_1102@outlook.com <amar_singh_1102@outlook.com>
2020-09-17 23:41:20 +0530

    1)Adding text-decoration property to all css modules. 2)Reducing search image size.

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,7 @@ import "./App.css";
 import { Button } from "./components/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <Button
-        href="https://www.google.com/"
-        buttonLabel="link"
-        onClick={() => {
-          console.log("Congrats for building your first reusbale component");
-        }}
-        buttonStyles="btn--success--solid"
-        type="button"
-        disabled={true}
-      ></Button>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
