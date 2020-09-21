dfc7f970c133609016b8d80b12899f69e3bd8afd
Amrendra Singh <45631761+Amar-dev-code@users.noreply.github.com>
2020-09-22 00:14:17 +0530

    Initial Commit for Login Form with Formik (#12)
    Co-authored-by: amar_singh_1102@outlook.com <amar_singh_1102@outlook.com>

---
+++ src/components/Molecules/Login.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useFormik } from "formik";
+import { TextBox } from "../../components/TextBox";
+export function Login() {
+  const formik = useFormik({
+    initialValues: {
+      userName: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <TextBox
+        type="Text"
+        placeholder="Enter username"
+        value={formik.values.userName}
+      ></TextBox>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
