cb40272ea48b3eaf49ea52d44702b774075d5cde
amar_singh_1102@outlook.com <amar_singh_1102@outlook.com>
2020-09-25 23:07:39 +0530

    Working on Login form

--- src/components/textbox.stories.tsx
+++ src/components/textbox.stories.tsx
@@ -5,15 +5,20 @@ import { TextBox } from "./TextBox";
 
 storiesOf("TextBox", module)
   .add("Textbox", () => (
-    <TextBox placeholder="Enter username" type="Text"></TextBox>
+    <TextBox placeholder="Enter username" type="Text" value=""></TextBox>
   ))
   .add("Password", () => (
-    <TextBox type="Password" placeholder="Enter Your Password"></TextBox>
+    <TextBox
+      type="Password"
+      placeholder="Enter Your Password"
+      value=""
+    ></TextBox>
   ))
   .add("Disabled", () => (
     <TextBox
       isDisabled={true}
       type="Text"
       placeholder="I am Disabled :("
+      value=""
     ></TextBox>
   ));
